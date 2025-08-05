cd1dffded3c2cfd36fa48485ef9d46069034f3d3
Apollo Lopes Damasceno Sá <apollolds2@gmail.com>
2025-08-05 10:51:03 -0300

    removendo arquivos desnecessários

--- create_db.py
+++
@@ -1,6 +0,0 @@
-from app.database import Base, engine
-
-print("Criando tabelas...")
-Base.metadata.drop_all(bind=engine)
-# Base.metadata.create_all(bind=engine)
-print("Tabelas criadas com sucesso!")
